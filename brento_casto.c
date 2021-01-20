c5396bc4fd81802736ab2d16b02ab13d98d4f74b
Matthew McKay KE0PSL <matt@mattmckay.org>
2021-01-20 09:14:13 -0600

    Add a comment

--- brento_casto.c
+++ brento_casto.c
@@ -4,6 +4,17 @@
 
 int main() {
 	uint32_t someFloatAsAnInt = 0x3F800000;
+	/*
+	Hello, Brento. This is a quick example of turnign your "hex value" into a float in C
+		float * someFloatAsAFloat
+			This defines a new pointer to a float value
+		(&someFloatAsAnInt)
+			This takes the memory address of the integer
+			This would be where your "hex value" is
+		(float *)
+			This "reinterperts" the memory address instead of 
+			being a "pointer to uint32" into "pointer to float"			    
+	*/
 	float * someFloatAsAFloat = (float *)(&someFloatAsAnInt);
 	printf("Hello, brento. As Hex: %08" PRIx32 ". As Float: %.6f\n", someFloatAsAnInt,*someFloatAsAFloat);
 	/*
